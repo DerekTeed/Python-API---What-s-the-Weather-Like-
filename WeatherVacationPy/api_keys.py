61541bbbed096f0939724459e66f28f9fc3397aa
Derek Teed <Derek@MacBook-Air.local>
2020-04-19 11:17:24 -0500

    update7

--- WeatherVacationPy/api_keys.py
+++ WeatherVacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "0bc9a902d2b0c6bd0c106b21c331c0c7"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyACW-H64oL1CzQnf9fdSwrLdqnDCgMwIIM"
